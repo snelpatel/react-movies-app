6c9a4f9190418b7bdeb0094e41930786562de2d3
Snehal Patel <snelpatel@gmail.com>
2021-03-30 11:46:44 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(<div></div>,document.getElementById('root')
+import Home from './screens/home/Home';
+ReactDOM.render(<Home />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
